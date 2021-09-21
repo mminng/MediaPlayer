047092a257c24f45028dfefe39dd2e81e1697f1e
zh <zh@gmail.com>
2021-09-21 18:54:02 +0800

    init

---
+++ library/src/main/java/com/github/mminng/media/Player.kt
@@ -0,0 +1,7 @@
+package com.github.mminng.media
+
+/**
+ * Created by zh on 2021/9/20.
+ */
+interface Player {
+}
---
+++ library/src/main/java/com/github/mminng/media/controller/PlayerControlView.kt
@@ -0,0 +1,14 @@
+package com.github.mminng.media.controller
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+/**
+ * Created by zh on 2021/9/20.
+ */
+class PlayerControlView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : FrameLayout(context, attrs) {
+
+}
